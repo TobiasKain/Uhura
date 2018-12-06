5e2fecaf7bd66d40023fc0b3fbcca9035e46a712
Tobias Kain <e1329088@student.tuwien.ac.at>
2018-12-06 23:12:11 +0100

    created maven project

--- src/at/tuwien/entity/asp/NewLine.java
+++
@@ -1,7 +0,0 @@
-package at.tuwien.entity.asp;
-
-/**
- * Created by tobiaskain on 27/04/2017.
- */
-public class NewLine extends AspRule {
-}
--- src/at/tuwien/gui/TranslationType.java
+++
@@ -1,9 +0,0 @@
-package at.tuwien.gui;
-
-/**
- * Created by tobiaskain on 21/04/2017.
- */
-public enum TranslationType {
-    AUTOMATIC,
-    MANUAL
-}
